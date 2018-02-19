c70f913f08a622d373b10fa3d658bf88deaf924d
MaJoy <MaJoy@192.168.25.13>
2018-02-18 23:12:20 -0300

    Adição do método toString na classe Phone

--- src/addresbookme/Contact.java
+++ src/addresbookme/Contact.java
@@ -13,14 +13,25 @@ import abstractdatatypes.*;
 public class Contact extends Person{
     private String address;
     private String email;
-    private Phone phoneNumber;
+    private Phone[] phoneNumber;
     
     public Contact (String firstName, String lastName, String cpf, Date date, 
-            String address, String email, Phone phoneNumber ){        
+            String address, String email, Phone[] phoneNumber ){        
         super(firstName, lastName, cpf, date);
         this.address = address;
         this.email = email;
         this.phoneNumber = phoneNumber;    
     } 
+    
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        int i;
+        for (i = 0; i < this.phoneNumber.length; i++) {
+            str.append(phoneNumber[i]);
+            str.append("\n");
+        }
+        return str.toString();
+    }   
     
 }
